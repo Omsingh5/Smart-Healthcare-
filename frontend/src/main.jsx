6470b65eba4496da64d0bbe83468f199624c460e
Omsingh Rajput <omsingrajput05@gmail.com>
2026-06-14 07:35:13 +0530

    fix: finalize buffer polyfill and remove browser-incompatible require

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx"; // ✅ This line should work now
-
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
